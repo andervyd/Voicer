ed724121173db9f7bbf925116e0f9ac64d9930bd
andervyd <andervyd@rambler.ru>
2020-05-03 18:24:21 +0300

    add PLAY/PAUSE, clear code

--- src/main/java/by/andervyd/Controller.java
+++ src/main/java/by/andervyd/Controller.java
@@ -1,26 +1,17 @@
 package by.andervyd;
 
-
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
-import javafx.stage.StageStyle;
-
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class Controller {
-
     @FXML
     private ResourceBundle resources;
     @FXML
@@ -38,14 +29,37 @@ public class Controller {
     @FXML
     private Button closeTest;
 
+
+    public static boolean buttonTrue;
+
     @FXML
     void initialize() {
 
+        // Close windows on click
         buttonClose.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<Event>() {
             public void handle(Event event) {
                 buttonClose.getScene().getWindow().hide();
             }
         });
 
+        // on PLAY and on PAUSE (change image)
+        buttonPlayPause.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+            ImageView image1 = new ImageView(new Image("/images/play.png",70,70,false,true));
+            ImageView image2 = new ImageView(new Image("/images/pause.png",70,70,false,true));
+
+            @Override
+            public void handle(MouseEvent event) {
+                if (buttonTrue) {
+                    System.out.println("OM");
+                    buttonPlayPause.setImage(image1.getImage());
+                    buttonTrue = false;
+                } else {
+                    buttonTrue = true;
+                    buttonPlayPause.setImage(image2.getImage());
+                }
+                event.consume();
+            }
+        });
     }
 }
--- src/main/java/by/andervyd/MainApp.java
+++ src/main/java/by/andervyd/MainApp.java
@@ -18,7 +18,6 @@ public class MainApp extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
         launch(args);
     }
